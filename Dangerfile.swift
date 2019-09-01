12e91ddfbb2bca6091d36b373a6b0165ff704ce3
Marek Fořt <marekfort@me.com>
2019-09-01 13:39:09 +0200

    Add danger file.

---
+++ Dangerfile.swift
@@ -0,0 +1,11 @@
+import Danger 
+let danger = Danger()
+
+let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
+
+let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
+let sourceChanges = allSourceFiles.first(where: { $0.hasPrefix("Sources") })
+
+if !changelogChanged && sourceChanges != nil {
+  warn("No CHANGELOG entry added.")
+}
