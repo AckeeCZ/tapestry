7ccd0622505c1764eb658ef66b5704fc0ecf1ec8
Marek Fořt <marekfort@me.com>
2019-10-02 19:35:26 +0200

    Add XCTestCase useful extensions.

--- Tests/TapestryKitTests/InitCommandTests.swift
+++ Tests/TapestryKitTests/InitCommandTests.swift
@@ -1,4 +1,5 @@
 import SPMUtility
+import Basic
 import TuistCore
 import XCTest
 @testable import TapestryCoreTesting
@@ -6,18 +7,29 @@ import XCTest
 
 final class InitCommandTests: XCTestCase {
     private var subject: InitCommand!
+    private var fileHandler: FileHandling!
     private var parser: ArgumentParser!
     
     override func setUp() {
         super.setUp()
+        fileHandler = try! MockFileHandler()
         parser = ArgumentParser.test()
         subject = InitCommand(parser: parser,
-                              fileHandler: try! MockFileHandler(),
+                              fileHandler: fileHandler,
                               printer: MockPrinter(),
                               exampleGenerator: MockExampleGenerator(),
                               gitController: MockGitController(),
                               system: MockSystem(),
                               packageGenerator: MockPackageGenerator())
+    }
+    
+    func test_run_when_the_directory_is_not_empty() throws {
+        let path = fileHandler.currentPath
+        try fileHandler.touch(path.appending(component: "dummy"))
+
+        let result = try parser.parse(["init", "--path", path.pathString])
+
+        XCTAssertThrownError(try subject.run(with: result), InitCommandError.nonEmptyDirectory(path))
     }
 }
 
