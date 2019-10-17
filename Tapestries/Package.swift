754a7d8982a79981e8074af7dc1bbfa1345c6c21
Marek Fořt <marek.fort@ackee.cz>
2019-10-17 21:17:54 +0200

    Add tapestries directory.

---
+++ Tapestries/Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Tapestries",
+    dependencies: [
+        // Tapestry
+        .package(path: "../"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", .upToNextMajor(from: "0.40.13")),
+    ]
+)
---
+++ Tapestries/README.md
@@ -0,0 +1,3 @@
+# Tapestries
+
+A description of this package.
