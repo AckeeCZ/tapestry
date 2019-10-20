913d2e6f2078f8843a96e3c7cf0f21a082a4a3df
Marek Fořt <marek.fort@ackee.cz>
2019-10-20 15:35:22 +0200

    Add dependencies compatibility checker.

--- Sources/TapestryGen/PackageController.swift
+++ Sources/TapestryGen/PackageController.swift
@@ -66,9 +66,13 @@ public final class PackageController: PackageControlling {
         try FileHandler.shared.inDirectory(path) {
             var environment = ProcessInfo.processInfo.environment
             environment[Constants.EnvironmentVariables.colouredOutput] = "true"
-            try System.shared.runAndPrint([toolPath.pathString] + arguments,
-                                   verbose: false,
-                                   environment: environment)
+            if tool == "tapestry" {
+                try System.shared.runAndPrint([toolPath.pathString] + arguments,
+                                       verbose: false,
+                                       environment: environment)
+            } else {
+                try System.shared.runAndPrint([toolPath.pathString] + arguments)
+            }
         }
     }
 }
