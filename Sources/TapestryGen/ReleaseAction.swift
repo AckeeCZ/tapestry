05fa25e5a8e2edb30536451530bdb22dc296aaec
Marek Fořt <marek.fort@ackee.cz>
2019-10-21 15:09:02 +0200

    Add ReleaseAction tests and documentation.

--- Sources/TapestryGen/ReleaseAction.swift
+++ Sources/TapestryGen/ReleaseAction.swift
@@ -1,25 +1,42 @@
+/// Describes individual release action
 public struct ReleaseAction {
     /// Order when the action gets executed.
     ///
-    /// - pre: Before the sources and resources build phase.
-    /// - post: After the sources and resources build phase.
+    /// - pre: Before commiting and tagging new version.
+    /// - post: After tcommiting and tagging new version.
     public enum Order {
+        /// Before commiting and tagging new version.
         case pre
+        /// After tcommiting and tagging new version.
         case post
     }
     
+    /// Describes dependencies manager that you can add to your release action
+    /// You can use this enum for `PredefinedAction.dependenciesCompatibility`
     public enum DependenciesManager: String {
+        /// Cococapods
         case cocoapods
+        /// Carthage
         case carthage
+        /// Swift Package Manager
         case spm
     }
     
+    /// You can choose one of `PredefinedAction`s that tapestry provides for you
+    /// You can list all using `tapestry actions` and run individual actions `tapestry action action-name`
     public enum PredefinedAction {
+        /// Updates version in `README.md` and `YourLibrary.podspec`
         case docsUpdate
+        /// Runs `tool` with given `arguments` from `Tapestries/Package.swift`
         case run(tool: String, arguments: [String])
+        /// Checks compatibility of your library with given dependencies managers
         case dependenciesCompatibility([DependenciesManager])
     }
     
+    /// Describes what should be run
+    ///
+    /// - custom: is run from root of your library
+    /// - predefined: Runs one of `PredefinedAction`s that is provided by tapestry
     public enum Action {
         case custom(tool: String, arguments: [String])
         case predefined(PredefinedAction)
@@ -28,6 +45,7 @@ public struct ReleaseAction {
     /// Release action order.
     public let order: Order
     
+    /// Action to run
     public let action: Action
     
     public init(order: Order,
---
+++ Tests/PackageDescriptionTests/ReleaseActionTests.swift
@@ -0,0 +1,37 @@
+import XCTest
+@testable import TapestryCoreTesting
+@testable import PackageDescription
+
+final class ReleaseActionTests: TapestryUnitTestCase {
+    func test_releaseAction_when_pre_and_custom() {
+        // Given
+        let subject = ReleaseAction(order: .pre, action: .custom(tool: "tool", arguments: ["args"]))
+        
+        // Then
+        XCTAssertCodable(subject)
+    }
+    
+    func test_releaseAction_when_post_and_custom() {
+        // Given
+        let subject = ReleaseAction(order: .post, action: .custom(tool: "tool", arguments: ["args"]))
+        
+        // Then
+        XCTAssertCodable(subject)
+    }
+    
+    func test_releaseAction_when_pre_and_docsUpdate() {
+        // Given
+        let subject = ReleaseAction(order: .pre, action: .predefined(.docsUpdate))
+        
+        // Then
+        XCTAssertCodable(subject)
+    }
+    
+    func test_releaseAction_when_post_and_docsUpdate() {
+        // Given
+        let subject = ReleaseAction(order: .post, action: .predefined(.docsUpdate))
+        
+        // Then
+        XCTAssertCodable(subject)
+    }
+}
