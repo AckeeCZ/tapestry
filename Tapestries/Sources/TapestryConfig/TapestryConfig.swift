44c2802e72f332c51614f69504bed9abd0b2cfdc
Marek Fořt <marek.fort@ackee.cz>
2019-10-25 10:29:56 +0200

    Add CHANGELOG to add files in TapestryConfig.

--- Tapestries/Sources/TapestryConfig/TapestryConfig.swift
+++ Tapestries/Sources/TapestryConfig/TapestryConfig.swift
@@ -3,6 +3,8 @@ import PackageDescription
 let config = TapestryConfig(
     release: Release(actions: [.pre(.docsUpdate),
                                .pre(.dependenciesCompatibility([.spm(.all)]))],
-                     add: ["README.md"],
+                     add: ["README.md",
+                           "CHANGELOG.md",
+                           "Tapestries/Package.resolved"],
                      commitMessage: "Version \(Argument.version)",
                      push: true))
