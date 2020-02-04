0e16725d6ed7567925a7041e056d0ac4bbb4a3b8
Marek Fořt <marek.fort@ackee.cz>
2020-02-04 21:50:49 +0100

    Add mint support to TapestryConfig.swift

--- TapestryConfig.swift
+++ TapestryConfig.swift
@@ -4,8 +4,14 @@ let config = TapestryConfig(
     release: Release(actions: [.pre(.docsUpdate),
                                .pre(.dependenciesCompatibility([.spm(.all)])),
                                // Mint support
-                               .pre(tool: "swift", arguments: ["build", "-c", "release", "--product", "PackageDescription"])],
+                                .pre(tool: "swift", arguments: ["build", "-c", "release", "--product", "PackageDescription"]),
+                                .pre(tool: "cp", arguments: [".build/release/libPackageDescription.dylib"]),
+                                .pre(tool: "cp", arguments: [".build/release/PackageDescription.swiftmodule"]),
+                                .pre(tool: "cp", arguments: [".build/release/PackageDescription.swiftdoc"])],
                      add: ["README.md",
-                           "CHANGELOG.md",],
+                           "CHANGELOG.md",
+                           "libPackageDescription.dylib",
+                           "PackageDescription.swiftmodule",
+                           "PackageDescription.swiftdoc",],
                      commitMessage: "Version \(Argument.version)",
                      push: false))
