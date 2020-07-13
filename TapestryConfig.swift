47137fa7a15fad46adf1ca274a7b2efe0c394774
Marek Fořt <marek.fort@ackee.cz>
2020-07-13 22:04:06 +0200

    Add github release to tapestry config.

--- TapestryConfig.swift
+++ TapestryConfig.swift
@@ -1,17 +1,24 @@
 import TapestryDescription
 
 let config = TapestryConfig(
-    release: Release(actions: [.pre(.docsUpdate),
-                               .pre(.dependenciesCompatibility([.spm(.all)])),
-                               // Mint support
-                                .pre(tool: "swift", arguments: ["build", "-c", "release", "--product", "TapestryDescription"]),
-                                .pre(tool: "cp", arguments: ["-rf", ".build/release/libTapestryDescription.dylib", "libTapestryDescription.dylib"]),
-                                .pre(tool: "cp", arguments: [".build/release/TapestryDescription.swiftmodule", "TapestryDescription.swiftmodule"]),
-                                .pre(tool: "cp", arguments: [".build/release/TapestryDescription.swiftdoc", "TapestryDescription.swiftdoc"])],
-                     add: ["README.md",
-                           "CHANGELOG.md",
-                           "libTapestryDescription.dylib",
-                           "TapestryDescription.swiftmodule",
-                           "TapestryDescription.swiftdoc",],
-                     commitMessage: "Version \(Argument.version)",
-                     push: true))
+    release: Release(
+        actions: [
+            .pre(.docsUpdate),
+            .pre(.dependenciesCompatibility([.spm(.all)])),
+            // Mint support
+            .pre(tool: "swift", arguments: ["build", "-c", "release", "--product", "TapestryDescription"]),
+            .pre(tool: "cp", arguments: ["-rf", ".build/release/libTapestryDescription.dylib", "libTapestryDescription.dylib"]),
+            .pre(tool: "cp", arguments: [".build/release/TapestryDescription.swiftmodule", "TapestryDescription.swiftmodule"]),
+            .pre(tool: "cp", arguments: [".build/release/TapestryDescription.swiftdoc", "TapestryDescription.swiftdoc"])],
+            .post(.githubRelease(owner: "AckeeCZ", repository: "tapestry")),
+        add: [
+            "README.md",
+            "CHANGELOG.md",
+            "libTapestryDescription.dylib",
+            "TapestryDescription.swiftmodule",
+            "TapestryDescription.swiftdoc",
+        ],
+        commitMessage: "Version \(Argument.version)",
+        push: true)
+)
+
