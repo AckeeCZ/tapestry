ba758d947b760e75e32523ac0e7d0acfe9a58dbc
Marek Fořt <marek.fort@ackee.cz>
2020-07-14 22:51:39 +0200

    Remove mint support.

--- TapestryConfig.swift
+++ TapestryConfig.swift
@@ -6,10 +6,10 @@ let config = TapestryConfig(
             .pre(.docsUpdate),
             .pre(.dependenciesCompatibility([.spm(.all)])),
             // Mint support
-            .pre(tool: "swift", arguments: ["build", "-c", "release", "--product", "TapestryDescription"]),
-            .pre(tool: "cp", arguments: ["-rf", ".build/release/libTapestryDescription.dylib", "libTapestryDescription.dylib"]),
-            .pre(tool: "cp", arguments: [".build/release/TapestryDescription.swiftmodule", "TapestryDescription.swiftmodule"]),
-            .pre(tool: "cp", arguments: [".build/release/TapestryDescription.swiftdoc", "TapestryDescription.swiftdoc"]),
+            // .pre(tool: "swift", arguments: ["build", "-c", "release", "--product", "TapestryDescription"]),
+            // .pre(tool: "cp", arguments: ["-rf", ".build/release/libTapestryDescription.dylib", "libTapestryDescription.dylib"]),
+            // .pre(tool: "cp", arguments: [".build/release/TapestryDescription.swiftmodule", "TapestryDescription.swiftmodule"]),
+            // .pre(tool: "cp", arguments: [".build/release/TapestryDescription.swiftdoc", "TapestryDescription.swiftdoc"]),
             .post(.githubRelease(owner: "AckeeCZ", repository: "tapestry")),
         ],
         add: [
