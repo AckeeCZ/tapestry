e27151b702fe983e0d9bd4fdc65e1e3c0aa9be48
Marek Fořt <marek.fort@ackee.cz>
2020-07-14 23:33:37 +0200

    Add mint support back.

--- TapestryConfig.swift
+++ TapestryConfig.swift
@@ -6,11 +6,11 @@ let config = TapestryConfig(
             .pre(.docsUpdate),
             .pre(.dependenciesCompatibility([.spm(.all)])),
             // Mint support
-            // .pre(tool: "swift", arguments: ["build", "-c", "release", "--product", "TapestryDescription"]),
-            // .pre(tool: "cp", arguments: ["-rf", ".build/release/libTapestryDescription.dylib", "libTapestryDescription.dylib"]),
-            // .pre(tool: "cp", arguments: [".build/release/TapestryDescription.swiftmodule", "TapestryDescription.swiftmodule"]),
-            // .pre(tool: "cp", arguments: [".build/release/TapestryDescription.swiftdoc", "TapestryDescription.swiftdoc"]),
-            // .post(.githubRelease(owner: "AckeeCZ", repository: "tapestry")),
+            .pre(tool: "swift", arguments: ["build", "-c", "release", "--product", "TapestryDescription"]),
+            .pre(tool: "cp", arguments: ["-rf", ".build/release/libTapestryDescription.dylib", "libTapestryDescription.dylib"]),
+            .pre(tool: "cp", arguments: [".build/release/TapestryDescription.swiftmodule", "TapestryDescription.swiftmodule"]),
+            .pre(tool: "cp", arguments: [".build/release/TapestryDescription.swiftdoc", "TapestryDescription.swiftdoc"]),
+            .post(.githubRelease(owner: "AckeeCZ", repository: "tapestry")),
         ],
         add: [
             "README.md",
